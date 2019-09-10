0dae5c95d059f34149bcd8afa8bf04a0ad10e426
Kaspars Dambis <hi@kaspars.net>
2019-09-10 11:16:35 +0300

    Use composer script to define the PHP linter params

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,23 +34,12 @@ module.exports = function( grunt ) {
 					urls: [ 'http://localhost:8000/tests/js/index.html?wp=' + grunt.option( 'wp' ) ]
 				}
 			}
-		},
-		phpcs: {
-			plugin: {},
-			options: {
-				bin: "vendor/bin/phpcs",
-				showSniffCodes: true,
-				standard: "phpcs.ruleset.xml",
-				verbose: true,
-				warningSeverity: 0,
-			}
-		},
+		}
 	});
 
 
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	grunt.loadNpmTasks( 'grunt-phpcs' );
 
 	grunt.task.run( 'connect' );
 
